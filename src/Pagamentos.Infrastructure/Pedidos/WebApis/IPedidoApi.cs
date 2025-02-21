879eb040d2bc6544c71a665ca2d40255c9c1d3ca
Felipe Bergmann <felipe.bergmann.f@gmail.com>
2025-02-21 02:19:53 -0300

    fix api

--- src/Pagamentos.Infrastructure/Pedidos/WebApis/IPedidoApi.cs
+++ src/Pagamentos.Infrastructure/Pedidos/WebApis/IPedidoApi.cs
@@ -4,7 +4,7 @@ namespace Pagamentos.Infrastructure.Pedidos.WebApis;
 
 public interface IPedidoApi
 {
-    [Post("/pedido/{pedidoId}/status-pagamento")]
+    [Post("/v1/pedido/{pedidoId}/status-pagamento")]
     Task<ApiResponse<object>> AtualizaPedidoStatusPagamento(Guid pedidoId,
         [Body] AtualizaPedidoStatusPagamentoRequest request, [Header("x-requestid")] string? requestId = null);
 }
